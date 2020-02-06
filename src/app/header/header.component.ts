e24ee9dcd7b51f46d278478b6207385534f59857
MaximusAlvarez <nikhilr.v2.0@gmail.com>
2020-02-06 09:48:48 +0530

    Added Logout Button

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 import { AuthService } from '../auth/auth.service';
+import { Router, NavigationEnd, Event} from '@angular/router';
+import { filter } from 'rxjs/operators';
+import { SESSION_STORAGE, WebStorageService } from 'angular-webstorage-service';
 
 @Component({
   selector: 'app-header',
@@ -7,8 +10,24 @@ import { AuthService } from '../auth/auth.service';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  list:boolean = false;
+  unreg:boolean = false;
+  constructor(private authservice: AuthService, private router: Router, @Inject(SESSION_STORAGE) private storage: WebStorageService) { 
+    this.router.events.subscribe((event:Event) => {
+      if(event instanceof NavigationEnd ){
+        if(event.url.includes('list'))
+          this.list = true;
+        if(event.url.includes('unreg'))
+          this.unreg = true;
+      }
+    });
+  }
 
-  constructor(private authservice: AuthService) { }
+  doLogout() {
+    this.authservice.user.next(null);
+    this.router.navigate(['/auth']);
+    this.storage.remove('token');
+  }
 
   isAuthenticated = false;
   isAdmin = false;
@@ -17,6 +36,5 @@ export class HeaderComponent implements OnInit {
       this.isAdmin = this.authservice.isAdmin;
     })
   }
-
 }
 
